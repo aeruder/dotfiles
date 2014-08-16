0177e57c79536bacb23763fb3011d56f3d86b64f
Andrew Ruder <andy@aeruder.net>
2014-08-16 13:34:07 -0500

    make ace-jump work as a motion

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -196,6 +196,7 @@ If the new path's directories does not exist, create them."
 (require-package 'ace-jump-mode)
 (define-key evil-normal-state-map (kbd "SPC") 'ace-jump-char-mode)
 (define-key evil-visual-state-map (kbd "SPC") 'ace-jump-char-mode)
+(define-key evil-motion-state-map (kbd "SPC") 'evil-ace-jump-char-mode)
 (setq ace-jump-mode-scope 'window)
 
 ;;;
