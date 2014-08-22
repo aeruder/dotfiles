fe355e79bea0e0911f4cca3f6988ce2151ac3784
Andrew Ruder <andrew.ruder@elecsyscorp.com>
2014-08-22 18:59:56 -0500

    start of c mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -308,3 +308,34 @@ If the new path's directories does not exist, create them."
   (let ((my-linum-current-line-number (line-number-at-pos)))
     ad-do-it))
 (ad-activate 'linum-update)
+
+;;; Make .h files use c++ mode
+(add-to-list 'auto-mode-alist
+             '("\\.[h]\\'" . c++-mode))
+
+;;; Indentation settings
+(add-hook 'c-mode-common-hook
+    '(c-mode-default))
+(defun c-mode-linux()
+    "Linux/u-boot indent settings"
+    (interactive)
+    (setq c-default-style "linux"
+          c-basic-offset 8
+          tab-width 8
+          indent-tabs-mode t))
+(defun c-mode-gnu()
+    "GNU indent settings"
+    (interactive)
+    (setq c-default-style "gnu"
+          c-basic-offset 2
+          tab-width 8
+          indent-tabs-mode t))
+(defun c-mode-default()
+    "My default indentation settings for C modes"
+    (interactive)
+    (setq c-default-style "linux"
+          c-basic-offset 4
+          tab-width 4
+          indent-tabs-mode nil))
+(setq-default tab-width 8
+              indent-tabs-mode nil)
