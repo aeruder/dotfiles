7a5493f820093c3353f2c3bd677204e10e1d6aa5
Andrew Ruder <andrew.ruder@elecsyscorp.com>
2014-10-13 14:05:43 -0500

    make evil keybindings work in ag mode

--- lisp/init-ag.el
+++ lisp/init-ag.el
@@ -1,2 +1,9 @@
 (require-package 'ag)
+
+(add-hook 'ag-mode-hook
+          (lambda ()
+            (define-key ag-mode-map "j" 'evil-next-line)
+            (define-key ag-mode-map "k" 'evil-previous-line)
+            (define-key ag-mode-map "n" 'evil-ex-search-next)
+            (define-key ag-mode-map "N" 'evil-ex-search-previous)))
 (provide 'init-ag)
